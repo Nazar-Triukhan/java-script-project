a4ed97854b304fda1b5ed0695a83f799f40ae23b
Nazar <tryuhann@gmail.com>
2026-02-04 18:57:13 +0200

    add

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,45 +1,44 @@
 const inputRef = document.querySelector('.footer__input');
 const btnRef = document.querySelector('.footer__btn');
 const backdropRef = document.querySelector('.footer__backdrop');
-const btnCloseRef = document.querySelector('.footer__modal-close')
-const formRef = document.querySelector('.footer__form')
+const btnCloseRef = document.querySelector('.footer__modal-close');
 
+btnRef.addEventListener('click', e => {
+  e.preventDefault();
 
-btnRef.addEventListener('click', (e) => {
-    e.preventDefault()
-if (inputRef.value.trim() !== '') {
-openModal();
-}
+  const emailText = inputRef.value.trim();
+
+  if (
+    emailText === '' ||
+    !emailText.includes('@gmail.com') ||
+    emailText.length <= 10
+  ) {
+    alert('неправильно введений ваш email');
+    return;
+  }
+
+  openModal();
 });
 
+backdropRef.addEventListener('click', e => {
+  if (e.target === e.currentTarget) {
+    closeModal();
+  }
+});
 
-backdropRef.addEventListener('click',(event) => {
-        if(event.target === event.currentTarget){
-                closeModal()
-        }
-})
+document.addEventListener('keydown', e => {
+  if (e.code === 'Escape') {
+    closeModal();
+  }
+});
 
-document.addEventListener('keydown',(event)=> {
-    if(event.code === 'Escape' ){
-               closeModal()
-    }
-    
-})
-
-btnCloseRef.addEventListener('click',() => {
-    closeModal()
-})
-
+btnCloseRef.addEventListener('click', closeModal);
 
 function openModal() {
-backdropRef.classList.remove('is__close');
-inputRef.value = ''
+  backdropRef.classList.remove('is__close');
+  inputRef.value = '';
 }
-
 
 function closeModal() {
-backdropRef.classList.add('is__close');
-// inputRef.value = ''
+  backdropRef.classList.add('is__close');
 }
-
-
