68b442a6ecfe2bc3d15f06b5498ed69ba1bfe2f3
Nazar <tryuhann@gmail.com>
2026-02-06 19:04:32 +0200

    add svg modal

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,50 +1,46 @@
-const inputRef = document.querySelector('.footer__input');
-const btnRef = document.querySelector('.footer__btn');
-const backdropRef = document.querySelector('.footer__backdrop');
-const btnCloseRef = document.querySelector('.footer__modal-close');
-const formRef = document.querySelector('.footer__form');
+document.addEventListener('DOMContentLoaded', () => {
+  const inputRef = document.querySelector('.footer__input');
+  const btnRef = document.querySelector('.footer__btn');
+  const backdropRef = document.querySelector('.footer__backdrop');
+  const btnCloseRef = document.querySelector('.footer__modal-close');
 
-// кнопка
-btnRef.addEventListener('click', e => {
-  e.preventDefault();
+  btnRef.addEventListener('click', e => {
+    e.preventDefault();
 
-  const emailText = inputRef.value.trim();
+    const emailText = inputRef.value.trim();
 
-  if (
-    emailText === '' ||
-    !emailText.includes('@gmail.com') ||
-    emailText.length <= 10
-  ) {
-    alert('неправильно введений ваш email');
-    return;
+    if (
+      emailText === '' ||
+      !emailText.includes('@gmail.com') ||
+      emailText.length <= 10
+    ) {
+      alert('неправильно введений ваш email');
+      return;
+    }
+
+    openModal();
+  });
+
+  backdropRef.addEventListener('click', event => {
+    if (event.target === event.currentTarget) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+      closeModal();
+    }
+  });
+
+  btnCloseRef.addEventListener('click', closeModal);
+
+  function openModal() {
+    backdropRef.classList.remove('is__close');
+    inputRef.value = '';
   }
 
-  openModal();
-});
-
-// клік по фону
-backdropRef.addEventListener('click', event => {
-  if (event.target === event.currentTarget) {
-    closeModal();
+  function closeModal() {
+    backdropRef.classList.add('is__close');
   }
 });
-
-// Escape
-document.addEventListener('keydown', event => {
-  if (event.code === 'Escape') {
-    closeModal();
-  }
-});
-
-// кнопка ✖
-btnCloseRef.addEventListener('click', closeModal);
-
-// функції
-function openModal() {
-  backdropRef.classList.remove('is__close');
-  inputRef.value = '';
-}
-
-function closeModal() {
-  backdropRef.classList.add('is__close');
-}
