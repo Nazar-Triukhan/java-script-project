e9beb6dfdb421ad474f7b64a44459f16c54a1a8f
Коля <135145397+Kolya4954@users.noreply.github.com>
2026-02-06 19:23:20 +0200

    finalFix

---
+++ src/js/three_number.js
@@ -0,0 +1,13 @@
+const inputBigestEl = document.querySelectorAll(".threeNumbers__input")
+const theBigestNumber = document.querySelector(".threeNumbers__text")
+let numbers = ["","",""]
+inputBigestEl.forEach((input) => {
+    input.addEventListener("change", (e) => {
+        numbers[input.dataset.index] = e.target.value
+
+        if (numbers[0] !== "" && numbers[1] !== "" && numbers[2] !== "") {
+            numbers.sort((a, b) => b-a)
+            theBigestNumber.textContent = `Найбільше число, яке ви ввели - ${numbers[0]}`
+        }
+    })
+})
